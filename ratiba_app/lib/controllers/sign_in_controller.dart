7d8fea0df94b459c1d89aa6aa458ad22a4181eec
mogaya <mogayainnocent@gmail.com>
2024-10-06 12:57:44 +0300

    Sign in controller complete

--- ratiba_app/lib/controllers/sign_in_controller.dart
+++ ratiba_app/lib/controllers/sign_in_controller.dart
@@ -8,6 +8,11 @@ class SignInController extends GetxController {
 
   var userId = 0.obs;
   var userName = ''.obs;
+  var userPhone = ''.obs;
+  var userEmail = ''.obs;
 
-  void updateUserId(int num) => userId.value = num;
+  void updateUserId(int id) => userId.value = id;
+  void updateUserName(String name) => userName.value = name;
+  void updateUserPhone(String phone) => userName.value = phone;
+  void updateUserEmail(String email) => userName.value = email;
 }
